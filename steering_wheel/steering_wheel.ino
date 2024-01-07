d027ef35fd77fb1dcd2c1a88e05ccc898848e1ce
ThatCoolCoder <ThatCoolCoder@users.noreply.github.com>
2024-01-07 21:26:01 +1030

    first steps

--- steering_wheel/steering_wheel.ino
+++ steering_wheel/steering_wheel.ino
@@ -2,6 +2,13 @@
 #include <math.h>
 #include <QuadratureEncoder.h>
 
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_HMC5883_U.h>
+
+/* Assign a unique ID to this sensor at the same time */
+Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
+
 // Inputs
 Encoders encoder(2,3);
 
@@ -11,13 +18,25 @@ void setup()
 {
 	// Setuzp Serial Monitor
 	Serial.begin(2000000);
+
+	sensor_t sensor;
+	mag.getSensor(&sensor);
+	Serial.println("------------------------------------");
+	Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+	Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+	Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+	Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" uT");
+	Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" uT");
+	Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" uT");  
+	Serial.println("------------------------------------");
+	Serial.println("");
 }
 
 
 void loop() {
 	unsigned long crntTime = millis();
 
-	send_to_python(encoder.getEncoderCount());
+	// send_to_python(encoder.getEncoderCount());
 	delay(10);
 }
 
